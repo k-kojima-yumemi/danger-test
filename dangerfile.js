fe997dd5dddac37b349c81153313186353766cf1
Kojima Kohei <125636684+k-kojima-yumemi@users.noreply.github.com>
2023-04-05 15:53:30 +0900

    message

--- dangerfile.js
+++ dangerfile.js
@@ -4,6 +4,7 @@ const github = danger.github
 const baseBranchName = github.pr.base.ref
 const branchName = github.pr.head.ref
 
+message(`baseBranchName = ${baseBranchName} branchName = ${branchName}`)
 if(baseBranchName === 'main' || baseBranchName === 'master'){
     // PR branch must start with hotfix or release
     if(!branchName.startsWith('hotfix/') || !branchName.startsWith('release/')){
